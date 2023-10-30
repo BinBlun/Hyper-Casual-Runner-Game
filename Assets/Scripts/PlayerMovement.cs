4c7c0fa725e9111ad5ed31e220802e3f9be3ac61
Duc Anh <binflinterfler292001@gmail.com>
2023-10-30 20:03:54 +0700

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float limitValue;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MovePlayer();
+        }
+    }
+
+    void MovePlayer()
+    {
+        //Calculate the x position
+        float halfScreen = Screen.width / 2;
+        float xPos = (Input.mousePosition.x - halfScreen) / halfScreen;
+        float finalXPos = Mathf.Clamp(xPos * limitValue, -limitValue, limitValue);
+
+        playerTransform.localPosition = new Vector3(finalXPos, 1, 0);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Hyper Casual Runner Game
+ 
